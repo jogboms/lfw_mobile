95f6367fe5511abd46c48bffaab5e63c97de4791
Jogboms <jeremiahogbomo@gmail.com>
2018-08-06 13:14:04 +0100

    refactor(constants): tweak colors

--- lib/constants/mk_colors.dart
+++ lib/constants/mk_colors.dart
@@ -4,7 +4,7 @@ import 'package:flutter/widgets.dart';
 class MkColors {
   MkColors._();
   static const _baseWhite = 0xFFFFFFFF;
-  static const _baseBlack = 0xFF232323;
+  static const _baseBlack = 0xFF121314;
   static const MaterialColor white = const MaterialColor(
     _baseWhite,
     const <int, Color>{
@@ -23,16 +23,16 @@ class MkColors {
   static const MaterialColor black = const MaterialColor(
     _baseBlack,
     const <int, Color>{
-      50: const Color(0xFFfafafa),
-      100: const Color(0xFFf5f5f5),
-      200: const Color(0xFFefefef),
-      300: const Color(0xFFe2e2e2),
-      400: const Color(0xFF444444),
-      500: const Color(0xFFa0a0a0),
-      600: const Color(0xFF777777),
-      700: const Color(0xFF636363),
-      800: const Color(0xFF444444),
-      900: const Color(_baseBlack),
+      50: const Color(0xFFf5f5f5),
+      100: const Color(0xFFefefef),
+      200: const Color(0xFFe2e2e2),
+      300: const Color(0xFFa0a0a0),
+      400: const Color(0xFF777777),
+      500: const Color(0xFF636363),
+      600: const Color(0xFF444444),
+      700: const Color(0xFF232323),
+      800: const Color(_baseBlack),
+      900: const Color(0xFF000000),
     },
   );
   static const Color accent = const Color(_baseWhite);
